2ad4cc3559e943fb28b1d17f2063db72f58b618f
InhyeKang <kanginhae0826@gmail.com>
2022-06-16 23:12:28 +0900

    [ADD] COMPLETE VCS

---
+++ DaemaIntheSoop/DaemaIntheSoop/VC/LogInVC.swift
@@ -0,0 +1,40 @@
+//
+//  LogInVC.swift
+//  DaemaIntheSoop
+//
+//  Created by 강인혜 on 2022/06/16.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+    @IBOutlet weak var txtFieldID: UITextField!
+    @IBOutlet weak var txtFieldPW: UITextField!
+    @IBAction func btnLogIn(_ sender: Any) {
+        guard let logInVC = self.storyboard?.instantiateViewController(identifier: "TabBarVC") as? TabBarVC else { return }
+        
+        logInVC.modalPresentationStyle = .fullScreen
+        
+        self.present(logInVC, animated: true, completion: nil)
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DaemaIntheSoop/DaemaIntheSoop/VC/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  DaemaIntheSoop
+//
+//  Created by 강인혜 on 2022/06/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var lbAppTitle: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+//        for family in UIFont.familyNames {
+//          print(family)
+//
+//          for sub in UIFont.fontNames(forFamilyName: family) {
+//            print("====> \(sub)")
+//          }
+//        }
+        
+        lbAppTitle.text = "Daema; In the Soop"
+        lbAppTitle.font = UIFont(name: "StretchProRegular", size: 55)
+    }
+
+
+}
+
