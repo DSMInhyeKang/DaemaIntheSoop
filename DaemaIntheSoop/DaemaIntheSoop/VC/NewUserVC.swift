4a19a28d5487e079f8fc5c77095b6a99e44e57af
InhyeKang <kanginhae0826@gmail.com>
2022-06-30 23:38:50 +0900

    [ADD] ALERT FOR CREATING NEW USER

--- DaemaIntheSoop/DaemaIntheSoop/VC/NewUserVC.swift
+++ DaemaIntheSoop/DaemaIntheSoop/VC/NewUserVC.swift
@@ -15,6 +15,7 @@ class NewUserVC: UIViewController {
     @IBOutlet weak var lbUserState: UILabel!
     
     
+   
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,11 +24,40 @@ class NewUserVC: UIViewController {
     
     @IBAction func btnCheckUser(_ sender: Any) {
         
-        let txtFieldName = self.txtFieldName.text
         let txtFieldUsername = self.txtFieldUsername.text
-        let txtFieldPW = self.txtFieldPW.text
         
         
+        let url = "http://35.216.6.254:8080/register"
+        var request = URLRequest(url: URL(string: url)!)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.timeoutInterval = 10
+        
+        // POST 로 보낼 정보
+        let params = [
+            "username": "txtFieldUsername"
+        ] as Dictionary
+
+        // httpBody 에 parameters 추가
+        do {
+            try request.httpBody = JSONSerialization.data(withJSONObject: params, options: [])
+        } catch {
+            print("http Body Error")
+        }
+        
+        AF.request(request).response { (response) in
+            print(response.request)
+            switch response.result {
+            case .success:
+                self.lbUserState.text = "사용 가능한 아이디입니다."
+            case .failure(let error):
+                self.lbUserState.text = "이미 존재하는 사용자입니다."
+            }
+        }
+    }
+    
+    
+    @IBAction func btnNewUser(_ sender: Any) {
         let url = "http://35.216.6.254:8080/register"
         var request = URLRequest(url: URL(string: url)!)
         request.httpMethod = "POST"
@@ -52,11 +82,20 @@ class NewUserVC: UIViewController {
             print(response.request)
             switch response.result {
             case .success:
-                self.lbUserState.text = "사용 가능한 아이디입니다."
+                let successOnAlert = UIAlertController(title: "안내", message: "회원가입 성공!", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "로그인 페이지로 돌아가기", style: UIAlertAction.Style.default, handler: nil)
+                
+                successOnAlert.addAction(onAction)
+                present(successOnAlert, animated: true, completion: nil)
             case .failure(let error):
-                self.lbUserState.text = "이미 존재하는 사용자입니다."
+                let failOnAlert = UIAlertController(title: "안내", message: "이미 존재하는 사용자입니다.", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "로그인 페이지로 돌아가기", style: UIAlertAction.Style.default, handler: nil)
+                
+                failOnAlert.addAction(onAction)
+                present(failOnAlert, animated: true, completion: nil)
             }
         }
-        
     }
+    
+    
 }
