110608cc8b60f253b3b5cf98ef710805d8e3a3e9
InhyeKang <kanginhae0826@gmail.com>
2022-07-05 17:37:16 +0900

    ♻️ :: 키체인 코드 수정

--- DaemaIntheSoop/DaemaIntheSoop/KeyChain/KeyChain.swift
+++ DaemaIntheSoop/DaemaIntheSoop/KeyChain/KeyChain.swift
@@ -8,11 +8,6 @@
 import Foundation
 import Security
 
-class Token {
-    static let accessToken = "access_token"
-    static let refreshToken = "refresh_token"
-}
-        
 class KeyChain {
     // Create
     class func create(key: String, token: String) {
@@ -21,12 +16,12 @@ class KeyChain {
             kSecAttrAccount: key,   // 저장할 Account
             kSecValueData: token.data(using: .utf8, allowLossyConversion: false) as Any   // 저장할 Token
         ]
-        SecItemDelete(query)
-        // Keychain은 Key값에 중복이 생기면, 저장할 수 없기 때문에 먼저 Delete해줌
-        let status = SecItemAdd(query, nil)
-        assert(status == noErr, "🥵 failed to save Token")
-    }
+        SecItemDelete(query)    // Keychain은 Key값에 중복이 생기면, 저장할 수 없기 때문에 먼저 Delete해줌
 
+        let status = SecItemAdd(query, nil)
+        assert(status == noErr, "failed to save Token")
+    }
+    
     // Read
     class func read(key: String) -> String? {
         let query: NSDictionary = [
@@ -35,21 +30,21 @@ class KeyChain {
             kSecReturnData: kCFBooleanTrue as Any,  // CFData 타입으로 불러오라는 의미
             kSecMatchLimit: kSecMatchLimitOne       // 중복되는 경우, 하나의 값만 불러오라는 의미
         ]
-
+        
         var dataTypeRef: AnyObject?
         let status = SecItemCopyMatching(query, &dataTypeRef)
-
+        
         if status == errSecSuccess {
             if let retrievedData: Data = dataTypeRef as? Data {
                 let value = String(data: retrievedData, encoding: String.Encoding.utf8)
                 return value
             } else { return nil }
         } else {
-            print("🥵 failed to loading, status code = \(status)")
+            print("failed to loading, status code = \(status)")
             return nil
         }
     }
-
+    
     // Delete
     class func delete(key: String) {
         let query: NSDictionary = [
@@ -57,6 +52,6 @@ class KeyChain {
             kSecAttrAccount: key
         ]
         let status = SecItemDelete(query)
-        assert(status == noErr, "🥵 failed to delete the value, status code = \(status)")
+        assert(status == noErr, "failed to delete the value, status code = \(status)")
     }
 }
