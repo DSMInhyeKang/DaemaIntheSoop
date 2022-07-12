5c915539cb09fe3b9c17de7cd1e13baa2136893c
InhyeKang <kanginhae0826@gmail.com>
2022-07-12 22:34:19 +0900

    🌧 :: 글 수정 데이터 구현

--- DaemaIntheSoop/DaemaIntheSoop/TabBarVC/DetailVC.swift
+++ DaemaIntheSoop/DaemaIntheSoop/TabBarVC/DetailVC.swift
@@ -17,7 +17,6 @@ class DetailVC: UIViewController {
     var postWriter: String = ""
     var txt: String = ""
     
-    
     var result: [MainPostModel] = []
 
     override func viewDidLoad() {
@@ -27,7 +26,22 @@ class DetailVC: UIViewController {
         txtViewContent.text = "\(txt)"
 //        getPostDetail()
     }
-
+    
+    
+    @IBAction func reviseBtn(_ sender: UIButton) {
+        guard let view = self.storyboard?.instantiateViewController(withIdentifier: "ReviseVC") as? ReviseVC else { return }
+        view.reviseTitle = "\(lbPostTitle.text!)"
+        view.reviseUser = "\(lbPostWriter.text!)"
+        view.reviseContent = "\(txtViewContent.text!)"
+        navigationController?.pushViewController(view, animated: true)
+    }
+    
+    
+    
+    
+    
+    
+    
 //    private func getPostDetail() {
 //        AF.request("http://35.216.6.254:8080/board/all", method: .get)
 //            .validate(statusCode: 200..<500)
@@ -47,29 +61,5 @@ class DetailVC: UIViewController {
 //    }
 
 //}
-//
-//extension MainVC: UITableViewDataSource, UITableViewDelegate {
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        return result.count
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath) as! PostListCell
-//        cell.lbUser.text = "\(result[indexPath.row].username)"
-//        cell.lbTitle.text = "\(result[indexPath.row].title)"
-//        return cell
-//    }
-//
-    
-    @IBAction func reviseBtn(_ sender: UIBarButtonItem) {
-        guard let view = self.storyboard?.instantiateViewController(withIdentifier: "ReviseVC") as? ReviseVC else { return }
-        
-//        view.reviseTitle = "\(lbPostTitle.text)"
-//        view.reviseUser = "\(lbPostWriter.text)"
-//        view.reviseContent = "\(txtViewContent.text)"
-//
-//        navigationController?.pushViewController(view, animated: true)
-    }
-    
 
 }
--- DaemaIntheSoop/DaemaIntheSoop/TabBarVC/ReviseVC.swift
+++ DaemaIntheSoop/DaemaIntheSoop/TabBarVC/ReviseVC.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 class ReviseVC: UIViewController {
     @IBOutlet weak var lbReviseTitle: UILabel!
@@ -23,15 +24,56 @@ class ReviseVC: UIViewController {
         txtViewReviseContent.text = "\(reviseContent)"
     }
     
+    @IBAction func reviseBtn(_ sender: UIButton) {
+        let lbReviseTitle = self.lbReviseTitle.text
+        let txtViewReviseContent = self.txtViewReviseContent.text
+        
+        
+        //전송할 값
+        let url = "http://35.216.6.254:8080/board"
+        var request = URLRequest(url: URL(string: url)!)
+        request.method = .patch
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.timeoutInterval = 10
+        
+        
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue( "Bearer \(KeyChain.read(key: "accessToken") ?? "")", forHTTPHeaderField: "Authorization")
+        
+        // POST 로 보낼 정보
+        let params = ["title" : lbReviseTitle!,
+                      "content" : txtViewReviseContent!] as Dictionary
+        
+        // httpBody 에 parameters 추가
+        do {
+            try request.httpBody = JSONSerialization.data(withJSONObject: params, options: [])
+            
+        } catch {
+            print("http Body Error")
+        }
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        AF.request(request).response { (response) in
+            print(response.request ?? "")
+            switch response.result {
+            case .success:
+                debugPrint(response)
+                let successOnAlert = UIAlertController(title: "안내", message: "게시글 수정 완료", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "홈 화면으로 돌아가기", style: UIAlertAction.Style.default, handler: nil)
+                
+                successOnAlert.addAction(onAction)
+                self.present(successOnAlert, animated: true, completion: nil)
+        
+                
+            case .failure(let error):
+                print(error)
+                let failOnAlert = UIAlertController(title: "안내", message: "게시글 수정 실패", preferredStyle: UIAlertController.Style.alert)
+                let onAction = UIAlertAction(title: "상세보기 화면으로", style: UIAlertAction.Style.default, handler: nil)
+                
+                failOnAlert.addAction(onAction)
+                self.present(failOnAlert, animated: true, completion: nil)
+            }
+        }
     }
-    */
-
+    
 }
