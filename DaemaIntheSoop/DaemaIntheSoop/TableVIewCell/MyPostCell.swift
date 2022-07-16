02419c8f65fcad8e56851bb0a0f2ac7a7574a243
InhyeKang <kanginhae0826@gmail.com>
2022-07-16 23:56:55 +0900

    🌱 :: 내 글 목록 셀 파일 생성

---
+++ DaemaIntheSoop/DaemaIntheSoop/TableVIewCell/MyPostCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyPostCell.swift
+//  DaemaIntheSoop
+//
+//  Created by 강인혜 on 2022/07/16.
+//
+
+import UIKit
+
+class MyPostCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
