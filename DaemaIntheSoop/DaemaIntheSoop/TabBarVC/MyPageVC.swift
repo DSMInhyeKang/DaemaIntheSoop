76b4c13dd3bbc4acf30f786edf59b7c8851494a7
InhyeKang <kanginhae0826@gmail.com>
2022-07-17 03:17:26 +0900

    🌧 :: 마이페이지 내 글 보기 기능 구현

--- DaemaIntheSoop/DaemaIntheSoop/TabBarVC/MyPageVC.swift
+++ DaemaIntheSoop/DaemaIntheSoop/TabBarVC/MyPageVC.swift
@@ -6,15 +6,22 @@
 //
 
 import UIKit
+import Alamofire
 
 class MyPageVC: UIViewController {
+    
     @IBOutlet weak var myPostsTableView: UITableView!
+    
+    var model: [Content] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        myPostsTableView.delegate = self
+        myPostsTableView.dataSource = self
+        
+        getMyPosts()
     }
+    
     
     @IBAction func btnLogOut(_ sender: Any) {
         guard let signOut = self.storyboard?.instantiateViewController(identifier: "LogInVC") as? LogInVC else { return }
@@ -23,14 +30,48 @@ class MyPageVC: UIViewController {
         self.present(signOut, animated: true, completion: nil)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func getMyPosts() {
+        
+        let url = "http://35.216.6.254:8080/board"
+        var request = URLRequest(url: URL(string: url)!)
+        request.method = .get
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+//        request.timeoutInterval = 10
+        
+        
+//        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue( "Bearer \(KeyChain.read(key: "accessToken") ?? "")", forHTTPHeaderField: "Authorization")
+        
+        
+        AF.request(request).response { (response) in switch response.result {
+                case .success:
+                    debugPrint(response)
+            
+                    if let data = try? JSONDecoder().decode(MainPostModel.self, from: response.data!){
+                        DispatchQueue.main.async {
+                            self.model = data.content
+                            self.myPostsTableView.reloadData()
+                        }
+                    }
+                case .failure(let error):
+                    print(error)
+                }
+            }
     }
-    */
+}
 
+
+extension MyPageVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return model.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myListCell", for: indexPath) as! MyPostCell
+        cell.lbMyPostTitle.text = "\(model[indexPath.row].title)"
+        return cell
+    }
 }
