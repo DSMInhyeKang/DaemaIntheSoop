fd9728c661fc8308ceace263e3fcf691b12f95fc
InhyeKang <kanginhae0826@gmail.com>
2022-08-04 21:17:45 +0900

    🌱 :: 모델 파일 생성

---
+++ DaemaIntheSoop/DaemaIntheSoop/Model/UserInfoModel.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfoModel.swift
+//  DaemaIntheSoop
+//
+//  Created by 강인혜 on 2022/08/04.
+//
+
+import Foundation
+
+struct UserInfoModel: Codable {
+    var username: String = .init()
+    var name: String = .init()
+}
