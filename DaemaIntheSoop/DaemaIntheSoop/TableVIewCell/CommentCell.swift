13e075c34ac6f1725744231496d3a41e46c11b54
InhyeKang <kanginhae0826@gmail.com>
2022-08-06 23:57:10 +0900

    🌱 :: 댓글 셀 파일 생성

---
+++ DaemaIntheSoop/DaemaIntheSoop/TableVIewCell/CommentCell.swift
@@ -0,0 +1,22 @@
+//
+//  CommentCell.swift
+//  DaemaIntheSoop
+//
+//  Created by 강인혜 on 2022/08/06.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+    @IBOutlet weak var lbComment: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
